b0f441d4752408df37abd198904001ec36eb67cd
Jakob Roedel <kabobrocks@Jakobs-iMac.home>
2017-10-11 13:47:39 -0700

    first commit

---
+++ config/Migrations/20171011202048_AddUserIdToArticles.php
@@ -0,0 +1,21 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddUserIdToArticles extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('articles');
+        $table->addColumn('user_id', 'integer', [
+            'limit' => 11
+        ])
+            ->update();
+    }
+}
